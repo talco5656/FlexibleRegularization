72168b12b8a4316b7ae536fb3e8250ff47798d92
Hyams <hyamsga@f8ffc26a9393.ant.amazon.com>
2020-09-22 23:04:12 +0300

    with PyTorch adaptive SGD optimizer; adtajst Pytorch.py script to run the adaptive optimizer; adapt Welford variance calculation to torch

--- PyTorch.py
+++ PyTorch.py
@@ -1043,7 +1043,7 @@ def test_seq():
                           momentum=0.9, nesterov=True, weight_decay=0.1, adaptive_weight_decay=True, iter_length=200)
 
     print("regular model:")
-    # train_part34(regular_model, optimizer)
+    train_part34(regular_model, optimizer)
     print("adaptive model:")
     train_part34(adaptive_model, adaptive_optimizer)
 
