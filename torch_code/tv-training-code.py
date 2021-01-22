4ae46f92feae3e618c0a61c64045ecd7af7413e1
hyamsga <galhyams@gmail.com>
2021-01-22 20:27:40 +0200

    refactor

--- torch_code/tv-training-code.py
+++ torch_code/tv-training-code.py
@@ -290,7 +290,7 @@ def parse_args():
     parser.add_argument("--inverse_var", default=1, type=int)
     parser.add_argument("--adaptive_avg_reg", default=0, type=int)
     # parser.add_argument("--mean_mean", default=0, type=int)
-    # parser.add_argument("--trains", default=1, type=int)
+    parser.add_argument("--trains", default=1, type=int)
     # parser.add_argument("--hidden_layers", default=5, type=int)
     # parser.add_argument("--lnn", default=0, type=int)
     # parser.add_argument("--reg_layers", default='1,2,3')
